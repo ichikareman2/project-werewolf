f93a405e2c740ed5f4e22bdad6467107f7e2b089
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-16 20:02:52 +0800

    init lobby component files

---
+++ client/src/modules/lobby/lobby.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lobby',
+  templateUrl: './lobby.component.html',
+  styleUrls: ['./lobby.component.css']
+})
+export class LobbyComponent {}
