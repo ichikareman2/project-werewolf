746fb4e49199c112ed0f7429841412c75018430d
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-16 20:05:25 +0800

    add lobby module in app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LobbyModule } from '../modules/lobby/lobby.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LobbyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
