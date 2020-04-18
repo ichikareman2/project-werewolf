12c7ee55f34a217706747ca1646f3acfc38d028f
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-18 14:10:11 +0800

    Feature/lobby routing (#9)
    * define lobby route
    
    * update main app to use routing
    
    * rename namespace

--- client/src/modules/lobby/lobby.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lobby',
-  templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.css']
-})
-export class LobbyComponent {}
