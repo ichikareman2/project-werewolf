a578bed8026923cdabe6f5e708c071dd62ce4fb7
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-18 17:04:44 +0800

    create seaprate component

---
+++ client/src/components/join-game/join-game.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'join-game',
+  templateUrl: './join-game.component.html',
+  styleUrls: ['./join-game.component.css']
+})
+export class JoinGameComponent {}
