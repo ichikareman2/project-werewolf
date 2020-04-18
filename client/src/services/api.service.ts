0662cd3a8fcce6b5cfc04c4f8dd5da9b8ed39fc3
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-18 21:26:25 +0800

    create api service

---
+++ client/src/services/api.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+    defaultOptions: {
+        observe: 'response',
+        responseType: 'json'
+    };
+
+    constructor(private http: HttpClient) { }
+
+    get(path: string, params: any | null) {
+        this.http.get(
+            `${environment.SERVER_ENDPOINT}${path}`,
+            {
+                ...this.defaultOptions,
+                params
+            }
+        );
+    }
+
+    post(path: string, params: any | null) {
+        this.http.post(
+            `${environment.SERVER_ENDPOINT}${path}`,
+            params,
+            this.defaultOptions
+        );
+    }
+}
