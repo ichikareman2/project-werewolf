cdcd6e0d56bc70a6942bb2db30e16dcab552aae1
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-18 14:02:05 +0800

    define lobby route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LobbyComponent } from '../modules/lobby/lobby.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lobby',
+    component: LobbyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
