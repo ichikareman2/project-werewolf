fb3214fac3055ffb12a4e71cf629fa50b3ac1212
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-18 14:42:20 +0800

    add lobby service in component

--- client/src/components/lobby/lobby.component.ts
+++ client/src/components/lobby/lobby.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LobbySocketService } from '../../services/lobby.service';
 
 @Component({
   selector: 'lobby',
   templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.css']
+  styleUrls: ['./lobby.component.css'],
+  providers: [LobbySocketService]
 })
-export class LobbyComponent {}
+export class LobbyComponent {
+  constructor(private lobbySocketService: LobbySocketService) {}
+}
