f655941ee2b7e4b6acb365153ffd2c981f1159bd
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-21 21:03:33 +0800

    messages file

---
+++ client/src/models/messages.ts
@@ -0,0 +1,19 @@
+import { GamePhaseEnum } from './game-phase';
+import { RolesEnum } from './player';
+
+const INSTRUCTION_MESSAGES = {
+    [GamePhaseEnum.DAY]: {
+        [RolesEnum.VILLAGER]: 'Discuss',
+        [RolesEnum.WEREWOLF]: 'Discuss',
+        [RolesEnum.SEER]: 'Discuss',
+    },
+    [GamePhaseEnum.NIGHT]: {
+        [RolesEnum.VILLAGER]: 'It\'s been a day. Sleep tight',
+        [RolesEnum.WEREWOLF]: 'Who would you like to eliminate tonight?',
+        [RolesEnum.SEER]: 'Whose role would you like to take a peak?'
+    }
+}
+
+export function getInstructionMessage( mode: GamePhaseEnum, role: RolesEnum ) {
+    return INSTRUCTION_MESSAGES[mode][role];
+}
