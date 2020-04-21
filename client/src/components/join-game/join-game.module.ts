ff7cf685e09f4844dd62442cd2c1b4f06097ed1a
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-21 17:04:05 +0800

    Feature/ui styles (#19)
    * image assets
    
    * banner component
    
    * add font
    
    * bg image
    
    * updates
    
    * refactor header module to show everywhere
    
    * make header smaller
    
    * style updates
    
    * rebase fix

--- client/src/components/join-game/join-game.module.ts
+++ client/src/components/join-game/join-game.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AlphanumericValidatorDirective } from '../../directives/alphanumeric-validator.directive';
+import { AlphanumericValidatorDirective } from 'src/directives/alphanumeric-validator.directive';
 import { JoinGameComponent } from './join-game.component';
 
 @NgModule({
