518daf6b1f05e5ced4ac1676c89ea0e4e23fda3c
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-21 20:00:44 +0800

    game component files

---
+++ client/src/components/game/game.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('game component');
+  }
+}
