9967460fa1be9e7522b9e3cadcf71c3f5e19cfc4
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-04-22 20:40:19 +0800

    display phase message

--- client/src/components/game/phase/phase.component.ts
+++ client/src/components/game/phase/phase.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { getPhaseMessage, GamePhaseEnum } from 'src/models';
 
 @Component({
   selector: 'game-phase',
   templateUrl: './phase.component.html',
   styleUrls: ['./phase.component.css']
 })
-export class GamePhaseComponent {
-  @Input() mode: string = '';
+export class GamePhaseComponent implements OnChanges {
+  @Input() mode: GamePhaseEnum = GamePhaseEnum.NIGHT;
+  phaseMessage: string = '';
+
+  ngOnChanges() {
+    this.phaseMessage = getPhaseMessage(this.mode);
+  }
 }
