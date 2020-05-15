812cad01010ecffd4c1efc51ab7367f612c91b28
kevin quing <willardquing@gmail.com>
2020-05-15 15:14:34 +0800

    Animation, optimization, bug fix
    * unsubscription
    * list animation
    * fix role image not changing
    * player comp onpush optimization. because it is pure enough

--- client/src/components/game/player/player.component.ts
+++ client/src/components/game/player/player.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ChangeDetectionStrategy, SimpleChanges } from '@angular/core';
 import { Player } from 'src/models';
 
 @Component({
   selector: 'game-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GamePlayerComponent implements OnInit, OnChanges {
   @Input() player: Player;
@@ -13,14 +14,15 @@ export class GamePlayerComponent implements OnInit, OnChanges {
   voteText: string;
 
   ngOnInit() {
-    this.roleImage = `assets/images/roles/${this.player.role.toLowerCase()}.png`;
   }
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
     if ( this.showVote ) {
       this.voteText = this.player.vote
         ? `Voted ${this.player.vote.name}`
         : 'Hasn\'t voted yet';
     }
+    const roleImage = `assets/images/roles/${this.player.role.toLowerCase()}.png`;
+    if (roleImage !== this.roleImage) { this.roleImage = roleImage }
   }
 }
